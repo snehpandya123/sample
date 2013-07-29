d191bce14a0c22bc6f66a1c555fa1b8575a0803f
sneh <sapandya92@yahoo.co.in>
2013-07-29 19:18:37 +0530

    changes to ruby 1.9 style

---
+++ app/controllers/example_user.rb
@@ -0,0 +1,12 @@
+example_user.rb
+class User
+	attr_accessor :name,:email
+	def initialize(attributes ={})
+	@name = attributes[:name]
+	@email=attributes[:email]
+	end
+	def formated_email
+	"#{@name} <#{@email}>"
+	end
+end
+
